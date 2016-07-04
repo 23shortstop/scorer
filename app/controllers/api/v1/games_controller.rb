67da1101b95e98ba36bbf779d396244a8b45db4a
Bohdan Nikolaienko <23bohdan@gmail.com>
2016-07-04 09:44:07 +0300

    Move getting an instance to a new method which is performed before an action.

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,10 +1,17 @@
 class Api::V1::GamesController < Api::V1::BaseController
+  before_action :set_game, only: [:show]
+
   def index
     render json: Game.all
   end
 
   def show
-    game = Game.find(params[:id])
-    render json: game
+    render json: @game
+  end
+
+  private
+
+  def set_game
+    @game = Game.find(params[:id])
   end
 end
