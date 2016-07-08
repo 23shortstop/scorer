3807f732060b164a1f8c7107449b129d87962e8a
Bohdan Nikolaienko <23bohdan@gmail.com>
2016-07-08 13:02:28 +0300

    Add tests for the Game Events model.

---
+++ spec/models/game_event_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe GameEvent, :type => :model do
+  subject { build :game_event }
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of :outcome }
+    it { is_expected.to validate_presence_of :player }
+    it { is_expected.to validate_presence_of :plate_appearance }
+  end
+
+  describe 'associations' do
+    it { is_expected.to belong_to :plate_appearance }
+    it { is_expected.to belong_to :player }
+  end
+
+  describe 'outcomes' do
+    it { is_expected.to define_enum_for(:outcome).
+      with([:assist, :put_out,
+            :single, :double, :triple, :home_run,
+            :safe_on_first, :safe_on_second, :safe_on_third, :scored,
+            :walk,
+            :sacrifice_fly, :sacrifice_bunt,
+            :force_out, :tag_out, :fly_out, :fielders_choice]) }
+  end
+end
