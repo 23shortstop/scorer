5c4e3ca9ff53ae411ed6ca61d9b79617f82763d6
Bohdan Nikolaienko <23bohdan@gmail.com>
2016-07-12 02:43:50 +0300

    Update the teams factory.

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :game do
-    date          { FFaker::Time.date }
+    date       { FFaker::Time.date }
     home_team  { build :team }
     away_team  { build :team }
     scorer     { build :scorer }
